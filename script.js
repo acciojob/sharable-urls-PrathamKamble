803d1be78dd2d2aef3e524fe8497dd70a20c557e
Pratham Kamble <91152849+PrathamKamble@users.noreply.github.com>
2023-11-30 17:57:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-// your code here
+const submit = document.getElementById('button');
+
+button.addEventListener('submit', () => {
+const url = document.getElementById('url');
+
+let location = window.location.search;
+
+url.innerHTML = location;
+	
+});
